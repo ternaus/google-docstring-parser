6c0e2a8ddde11e016514a3c11be2d957e87c0628
Vladimir Iglovikov <iglovikov@gmail.com>
2025-03-19 13:55:07 -0700

    fix in init

--- google_docstring_parser/__init__.py
+++ google_docstring_parser/__init__.py
@@ -4,5 +4,3 @@ A lightweight, efficient parser for Google-style Python docstrings that converts
 """
 
 from .google_docstring_parser import parse_google_docstring
-
-__all__ = ["parse_google_docstring"]
